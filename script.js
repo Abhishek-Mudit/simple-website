180fed7e8a77c8e9e67a99d6f6c50287f49e1785
Abhishek-Mudit <125530764+Abhishek-Mudit@users.noreply.github.com>
2024-02-01 23:30:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var person = {
+    name:"John" , 
+    age:19,
+    favColor:"blue",
+    height: 200
+};
+var x = person.name;
+var y = person.age;
+var z = person.favColor;
+var w =person['height'];
+
+docunment.write(x);
